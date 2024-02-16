e30d622b8fbf3e1f136abbbfee18decc51af42c6
Tabishbostami <tabish.bostami042000@gmail.com>
2024-02-16 17:58:06 +0530

    factorial code update

--- src/Factorial.java
+++ src/Factorial.java
@@ -1,3 +1,19 @@
-public class Factorial {
-    
+import java.util.Scanner;
+
+public class Factorial
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int nfact =1;
+        for(int i=n;i!=1;i--)
+        {
+
+            nfact = nfact * i ;
+
+        }
+
+        System.out.println(nfact);
+    }
 }
