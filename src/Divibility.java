91de41a251805d62daad9a7d72e22744d344b547
Tabishbostami <tabish.bostami042000@gmail.com>
2024-02-19 18:08:53 +0530

    code update

---
+++ src/Divibility.java
@@ -0,0 +1,12 @@
+public class Divibility {
+    public static void main(String[] args) {
+        int a=1;
+        while(a<=100){
+            if(a%3==0 && a%5==0){
+                System.out.println(a);
+            }
+            a++;
+        }
+
+    }
+}
