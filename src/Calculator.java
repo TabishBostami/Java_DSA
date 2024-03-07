27b2ca0060751f2495ab8f5eb325b1f826b80ddc
Tabishbostami <tabish.bostami042000@gmail.com>
2024-03-07 17:03:09 +0530

    add 2d array

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,7 +2,12 @@ import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+
+        String a="Tabish";
+        String b="Tabish bostami";
+
+        System.out.println(a + System.identityHashCode(a));
+        System.out.println(b + System.identityHashCode(b));
 
 
     }
