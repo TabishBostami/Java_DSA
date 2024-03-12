17dcf8ded1fefcd1441ec86bc00faf62c6c3f738
Tabishbostami <tabish.bostami042000@gmail.com>
2024-03-12 20:22:03 +0530

    add 2d array

---
+++ src/Array.java
@@ -0,0 +1,21 @@
+public class Array {
+    public static void main(String[] args) {
+        int [][] arr = {
+                {11, 12, 13, 34},
+                {15, 26, 74, 38},
+                {59, 10, 21, 12}
+        };
+
+
+        System.out.println(arr.length);
+        System.out.println(arr[1].length);
+
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[i].length;j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+
+        }
+    }
+}
