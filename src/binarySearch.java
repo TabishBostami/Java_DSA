9378acf8389773a958e339a2a9eba54b308cc395
Tabishbostami <tabish.bostami042000@gmail.com>
2024-05-16 18:19:32 +0530

    binarySearch

--- src/binarySearch.java
+++ src/binarySearch.java
@@ -1,7 +1,7 @@
 public class binarySearch {
     public static void main(String[] args) {
-        int[] arr = {3, 5, 6, 8, 9, 14, 11, 17, 18};
-        int ele = 8;
+        int[] arr = {3, 5, 6, 8, 9, 14, 11, 17, 18,25,27,29,34,37,38,39,44,47,49};
+        int ele = 25;
         int first =0;
         int last = arr.length;
         int count = 0;
@@ -9,8 +9,8 @@ public class binarySearch {
         while(first <= last){
             int mid = (first+last)/2;
             if(ele == arr[mid]){
-                System.out.println("Element is Found!!! in index "+ (mid+1) );
-                System.out.println("Total number of iteration "+count);
+                System.out.println("Element is Found!!! in index "+ (mid) );
+                System.out.println("Total number of iteration "+(count+1));
 
                 return;
             }
